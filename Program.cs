d3684028b175e86d371e0a64d2b6fb86e5171c97
ITHanan <155847905+ITHanan@users.noreply.github.com>
2025-03-16 19:17:14 +0100

    done

--- Program.cs
+++ Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        public static bool CanJump(int[]nums )
+        { 
+        int maxreach = 0;
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                if (i > maxreach)
+                
+                 return false;
+
+                 maxreach = Math.Max(maxreach, i + nums[i]);
+
+                if(maxreach>= nums.Length-1)
+                    return true;
+                
+            
+            
+            }
+        
+        return false;
+        }
         static void Main(string[] args)
         {
+            int[] nums1 = { 2, 3, 1, 1, 4 };
+            Console.WriteLine(CanJump(nums1));
 
+            int[] nums2 = { 3, 2, 1, 0, 4 };
+            Console.WriteLine(CanJump(nums2));
         }
     }
 }
